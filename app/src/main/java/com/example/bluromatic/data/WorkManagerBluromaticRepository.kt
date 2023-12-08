8fdfc008eee7e2bd36e19f3b214d883985c8862a
Nuray <210103331@stu.sdu.edu.kz>
2023-12-09 02:24:51 +0600

    tag and update UI

--- app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
+++ app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
@@ -18,6 +18,7 @@ package com.example.bluromatic.data
 
 import android.content.Context
 import android.net.Uri
+import androidx.lifecycle.asFlow
 import androidx.work.Data
 import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
@@ -33,14 +34,18 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import androidx.work.ExistingWorkPolicy
 import com.example.bluromatic.IMAGE_MANIPULATION_WORK_NAME
+import com.example.bluromatic.TAG_OUTPUT
+import kotlinx.coroutines.flow.mapNotNull
 
 class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
 
     private var imageUri: Uri = context.getImageUri()
     private val workManager = WorkManager.getInstance(context)
 
-    override val outputWorkInfo: Flow<WorkInfo?> = MutableStateFlow(null)
-
+    override val outputWorkInfo: Flow<WorkInfo> =
+        workManager.getWorkInfosByTagLiveData(TAG_OUTPUT).asFlow().mapNotNull {
+           if (it.isNotEmpty()) it.first() else null
+        }
     /**
      * Create the WorkRequests to apply the blur and save the resulting image
      * @param blurLevel The amount to blur the image
@@ -58,6 +63,7 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
 
         continuation = continuation.then(blurBuilder.build())
         val save = OneTimeWorkRequestBuilder<SaveImageToFileWorker>()
+            .addTag(TAG_OUTPUT)
             .build()
         continuation = continuation.then(save)
 
@@ -80,4 +86,5 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
         builder.putString(KEY_IMAGE_URI, imageUri.toString()).putInt(KEY_BLUR_LEVEL, blurLevel)
         return builder.build()
     }
+
 }
